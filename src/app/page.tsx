04c7e9eca8d4097bd1c5b01deea58d6332f0ca51
Eine <eined3v@gmail.com>
2024-07-19 04:44:36 +1000

    info edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,53 @@ import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
+  interface projectInfo {
+    name: string | undefined,
+    liveSiteUrl: string | undefined,
+    sourceCodeUrl: string | undefined,
+    summary: string | undefined;
+    retrospective: string | undefined;
+    previewImgUrl: string | undefined;
+  };
+  
+  const projectsFEM: projectInfo[] = [
+    {
+      name: 'REST Countries API with color theme switcher',
+      liveSiteUrl: '/rest-countries-api',
+      sourceCodeUrl: 'https://github.com/einedev/frontend-practice/tree/main/src/app/rest-countries-api',
+      summary: undefined,
+      retrospective: undefined,
+      previewImgUrl: undefined,
+    },
+    {
+      name: 'ip-address-tracker',
+      liveSiteUrl: '/ip-address-tracker',
+      sourceCodeUrl: undefined,
+      summary: undefined,
+      retrospective: undefined,
+      previewImgUrl: undefined,
+    }
+  ];
+
   return (
-    <main className={styles.main}>
-      {/* <h1>Frontend Mentor Projects</h1>
+    <div className={styles.main}>
+      <h1>Welcome!</h1>
+      <p>(wip...)</p>
+      <h2>Frontend Mentor Projects</h2>
+      <Link href="https://www.frontendmentor.io/profile/einedev">My profile on Frontend Mentor</Link>
+      <hr />
       <ul>
-        <li><Link href="/rest-countries-api">REST Countries API with color theme switcher</Link></li>
-      </ul> */}
-    </main>
+        {projectsFEM.map((proj, index) => (
+          <li>{proj.name}
+            <ul>
+            {proj.liveSiteUrl ? <li><Link href={proj.liveSiteUrl}>Live site</Link></li> : <></>}
+            {proj.sourceCodeUrl ? <li><Link href={proj.sourceCodeUrl}>Source code</Link></li> : <></>}
+            </ul>
+          </li>
+        ))}
+      </ul>
+      <h2>Others</h2>
+      <p>(wip...)</p>
+    </div>
   );
 }
